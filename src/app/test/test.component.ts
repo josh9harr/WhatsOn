6a69a9b0047a09f91705aac837a4066573a09a51
josh9harr <josh9harr@gmail.com>
2020-01-19 18:15:43 -0700

    made test component and printed from database

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CRUDService } from '../crud.service';
+import { Users } from 'src/assets/Users.model';
+// import { from } from 'rxjs';
+// import { create } from 'domain';
 
 @Component({
   selector: 'app-test',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+  users: Users[];
 
-  constructor() { }
+
+  constructor(private CRUDService: CRUDService) { }
 
   ngOnInit() {
+    this.CRUDService.getUser().subscribe(data => {
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Users
+      })
+      console.log(data)
+    });
   }
+
+    create(user: Users){
+      this.CRUDService.createUser(user);
+    }
+
+    update(user: Users){
+      this.CRUDService.updateUser(user)
+    }
+
+    delete(id: string){
+      this.CRUDService.deleteUser(id)
+    }
+
+
 
 }
